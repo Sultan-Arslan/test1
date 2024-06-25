a253239d35145daa925ee9edb4a867268da9d1a6
Arslan <for.deagle@bk.ru>
2024-06-25 15:45:19 +0500

    completed 95%

---
+++ my-laravel-app/resources/lang/kz/profile.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Profile' => 'Profile',
+    'Profile Information' => 'Profile Information',
+    "Update your account's profile information and email address." => "Update your account's profile information and email address.",
+    'save' => 'save',
+    'Update Password' => 'Update Password',
+    'Ensure your account is using a long, random password to stay secure.' => 'results',
+    'Current Password' => 'Current Password',
+    'results' => 'results',
+    'New Password' => 'New Password',
+    'Confirm Password' => 'Confirm Password',
+    'Delete Account' => 'Delete Account',
+    'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.'
+    => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+];
+?>
